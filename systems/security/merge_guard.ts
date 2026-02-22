cdd5542e80991e0231da5fa8a8da490bc9e964b7
Jay Haslam <jakerslam@gmail.com>
2026-02-21 23:54:00 -0700

    security: enforce memory and workspace dump guards in merge gate

--- systems/security/merge_guard.ts
+++ systems/security/merge_guard.ts
@@ -58,6 +58,8 @@ function runGuard(opts = {}) {
   checks.push(runCmd('contract_check', 'node', ['systems/spine/contract_check.js']));
   checks.push(runCmd('schema_contract_check', 'node', ['systems/security/schema_contract_check.js', 'run']));
   checks.push(runCmd('adaptive_layer_guard_strict', 'node', ['systems/sensory/adaptive_layer_guard.js', 'run', '--strict']));
+  checks.push(runCmd('memory_layer_guard_strict', 'node', ['systems/memory/memory_layer_guard.js', 'run', '--strict']));
+  checks.push(runCmd('workspace_dump_guard_strict', 'node', ['systems/security/workspace_dump_guard.js', 'run', '--strict']));
   checks.push(runCmd('repo_hygiene_guard_strict', 'node', ['systems/security/repo_hygiene_guard.js', 'run', '--strict']));
   if (!options.skipTests) {
     checks.push(runCmd('test_ci', 'npm', ['run', 'test:ci']));
