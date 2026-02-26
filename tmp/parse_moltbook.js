6664efced6e58c1c0142fc87798cdf15c15aa22f
Jay Haslam <jakerslam@gmail.com>
2026-02-26 15:43:21 -0700

    chore: reduce git churn by untracking generated runtime artifacts

--- tmp/parse_moltbook.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const fs = require('fs');
-const data = fs.readFileSync(0, 'utf8');
-const json = JSON.parse(data);
-if (json.success && json.posts) {
-  json.posts.slice(0, 5).forEach((p, i) => {
-    console.log(`${i+1}. [${p.upvotes}] ${p.title} by @${p.author.name} (id: ${p.id})`);
-  });
-}
